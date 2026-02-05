0e76c4ceb06fc2ff15933d4e2d5c3fcf492dd96b
Alexandru Iuga <alex.iuga@gmail.com>
2026-02-05 17:57:15 +0200

    Extracted setup class

--- sources/ClockWpf.TemplateEditor/App.xaml.cs
+++ sources/ClockWpf.TemplateEditor/App.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System.Windows;
-using DustInTheWind.ClockWpf.Templates;
-using DustInTheWind.ClockWpf.Movements;
-using Microsoft.Extensions.DependencyInjection;
 using DustInTheWind.ClockWpf.TemplateEditor.Presentation;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DustInTheWind.ClockWpf.TemplateEditor;
 
@@ -15,68 +13,24 @@ public partial class App : Application
     {
         base.OnStartup(e);
 
+        IServiceProvider serviceProvider = ConfigureServices();
+
+        MainWindow = CreateAndShowMainWindow(serviceProvider);
+    }
+
+    private static IServiceProvider ConfigureServices()
+    {
         ServiceCollection serviceCollection = new();
+        Setup.ConfigureServices(serviceCollection);
+        return serviceCollection.BuildServiceProvider();
+    }
 
-        ApplicationState applicationState = CreateApplicationState();
-        serviceCollection.AddSingleton(applicationState);
-
-        serviceCollection.AddTransient<MainWindow>();
-        serviceCollection.AddTransient<MainViewModel>();
-
-        IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
-
+    private static MainWindow CreateAndShowMainWindow(IServiceProvider serviceProvider)
+    {
         MainWindow mainWindow = serviceProvider.GetService<MainWindow>();
         mainWindow.DataContext = serviceProvider.GetService<MainViewModel>();
         mainWindow.Show();
 
-        MainWindow = mainWindow;
-    }
-
-    private static ApplicationState CreateApplicationState()
-    {
-        ApplicationState applicationState = new();
-
-        LoadTemplates(applicationState);
-        LoadMovements(applicationState);
-
-        return applicationState;
-    }
-
-    private static void LoadTemplates(ApplicationState applicationState)
-    {
-        List<Type> templateTypes = typeof(ClockTemplate).Assembly.GetTypes()
-            .Where(x => x.IsClass && !x.IsAbstract && typeof(ClockTemplate).IsAssignableFrom(x))
-            .ToList();
-
-        applicationState.AvailableTemplateTypes = templateTypes;
-
-        if (templateTypes.Count > 0)
-        {
-            Type selectedTemplateType = templateTypes
-                .FirstOrDefault(x => x == typeof(DefaultTemplate));
-
-            applicationState.CurrentTemplate = (ClockTemplate)Activator.CreateInstance(selectedTemplateType);
-        }
-    }
-
-    private static void LoadMovements(ApplicationState applicationState)
-    {
-        List<Type> movementTypes = typeof(IMovement).Assembly.GetTypes()
-            .Where(x => x.IsClass && !x.IsAbstract && typeof(IMovement).IsAssignableFrom(x))
-            .ToList();
-
-        applicationState.AvailableMovementTypes = movementTypes;
-
-        if (movementTypes.Count > 0)
-        {
-            Type selectedMovementType = movementTypes
-                .FirstOrDefault(x => x == typeof(LocalTimeMovement));
-
-            IMovement movement = (IMovement)Activator.CreateInstance(selectedMovementType);
-            movement.Start();
-
-            applicationState.CurrentMovement = movement;
-        }
+        return mainWindow;
     }
 }
-
---
+++ sources/ClockWpf.TemplateEditor/Setup.cs
@@ -0,0 +1,65 @@
+﻿using DustInTheWind.ClockWpf.Movements;
+using DustInTheWind.ClockWpf.TemplateEditor.Presentation;
+using DustInTheWind.ClockWpf.Templates;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DustInTheWind.ClockWpf.TemplateEditor;
+
+internal static class Setup
+{
+    public static void ConfigureServices(ServiceCollection serviceCollection)
+    {
+        ApplicationState applicationState = CreateApplicationState();
+        serviceCollection.AddSingleton(applicationState);
+
+        serviceCollection.AddTransient<MainWindow>();
+        serviceCollection.AddTransient<MainViewModel>();
+    }
+
+    private static ApplicationState CreateApplicationState()
+    {
+        ApplicationState applicationState = new();
+
+        LoadTemplates(applicationState);
+        LoadMovements(applicationState);
+
+        return applicationState;
+    }
+
+    private static void LoadTemplates(ApplicationState applicationState)
+    {
+        List<Type> templateTypes = typeof(ClockTemplate).Assembly.GetTypes()
+            .Where(x => x.IsClass && !x.IsAbstract && typeof(ClockTemplate).IsAssignableFrom(x))
+            .ToList();
+
+        applicationState.AvailableTemplateTypes = templateTypes;
+
+        if (templateTypes.Count > 0)
+        {
+            Type selectedTemplateType = templateTypes
+                .FirstOrDefault(x => x == typeof(DefaultTemplate));
+
+            applicationState.CurrentTemplate = (ClockTemplate)Activator.CreateInstance(selectedTemplateType);
+        }
+    }
+
+    private static void LoadMovements(ApplicationState applicationState)
+    {
+        List<Type> movementTypes = typeof(IMovement).Assembly.GetTypes()
+            .Where(x => x.IsClass && !x.IsAbstract && typeof(IMovement).IsAssignableFrom(x))
+            .ToList();
+
+        applicationState.AvailableMovementTypes = movementTypes;
+
+        if (movementTypes.Count > 0)
+        {
+            Type selectedMovementType = movementTypes
+                .FirstOrDefault(x => x == typeof(LocalTimeMovement));
+
+            IMovement movement = (IMovement)Activator.CreateInstance(selectedMovementType);
+            movement.Start();
+
+            applicationState.CurrentMovement = movement;
+        }
+    }
+}
